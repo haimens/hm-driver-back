f8403110c02c3b9ba9d5555efa6bf3a230ddc3b6
od3 <chrisyao900908@gmail.com>
2019-07-14 14:43:53 -0700

    SAVE FOR ALL

--- actions/sms.action.js
+++ actions/sms.action.js
@@ -27,12 +27,12 @@ class VNSMSAction {
             const {customer_token} = params;
             const {realm_token} = auth;
 
-            console.log(body);
+
             if (!customer_token) func.throwErrorWithMissingParam('customer_token');
             return coreConn.coreRequest(
                 'POST',
                 ['message', 'send', 'customer', realm_token, customer_token],
-                {}, {...body, type: 2}
+                {}, {}, {...body, type: 2}
             );
         } catch (e) {
             throw e;
